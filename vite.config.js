874482cc20778045c9123d74d78013fbccd0a771
Harsh Jaiswal <harshjaiswal.prgm@gmail.com>
2025-08-05 10:41:06 +0530

    warning fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    chunkSizeWarningLimit: 1024, // Suppress warning up to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          framer: ['framer-motion'],
+          radix: ['@radix-ui/react-dropdown-menu'],
+          ui: ['@shadcn/ui'],
+          chart: ['chart.js'],
+          icons: ['lucide-react'],
+        }
+      }
+    }
+  },
 })
