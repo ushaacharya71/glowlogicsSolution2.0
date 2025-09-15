84fc06a99bdb2031c7a12460a39046e671cd1dde
Harsh Jaiswal <harshjaiswal.prgm@gmail.com>
2025-09-15 15:10:27 +0530

    Update h1@G

--- src/pages/EventHub.jsx
+++ src/pages/EventHub.jsx
@@ -133,7 +133,7 @@ const EventHub = () => {
           </p>
           <div className="org-footer">
             <span>⭐ 5.0</span>
-            <span>Open for Registration</span>
+            <span>Registration started soon</span>
           </div>
         </div>
 
@@ -165,7 +165,7 @@ const EventHub = () => {
           </p>
           <div className="org-footer">
             <span>⭐ 4.7</span>
-            <span>Upcoming in August</span>
+            <span>Upcoming in December</span>
           </div>
         </div>
 
@@ -178,7 +178,7 @@ const EventHub = () => {
           </p>
           <div className="org-footer">
             <span>⭐ 4.5</span>
-            <span>July 18</span>
+            <span>Jan 18</span>
           </div>
         </div>
 
@@ -207,7 +207,7 @@ const EventHub = () => {
           </p>
           <div className="org-footer">
             <span>⭐ 4.9</span>
-            <span>Free - Aug 3</span>
+            <span>Free - Jan 3</span>
           </div>
         </div>
 
@@ -286,8 +286,8 @@ const EventHub = () => {
           Admiration expressed by others regarding our presence.
         </h2>
 
-        <div className="relative w-full space-y-8 overflow-x-hidden">
-          <div className="flex w-max animate-marquee space-x-6 px-6 items-start">
+        <div className="relative w-full space-y-8 overflow-x-hidden group">
+            <div className="flex w-max animate-marquee space-x-6 px-6 items-start animation-running hover:[animation-play-state:paused]">
             {[...Array(2)].flatMap(() =>
               [
                 {
@@ -337,12 +337,6 @@ const EventHub = () => {
                     title: "Jayaraj Annapackiam College",
                     text: "Hello everyone, I am Tejaswini K. I study Computer Science at Jayaraj Annapackiam College. I recently completed the Artificial Intelligence course from Glowlogics. The course was well-organized and beginner-friendly. The instructors explained everything in a clear manner. I now feel confident working on AI projects, especially using Python. Thank you, Glowlogics!",
                     img: "/TejasvaniK.png",
-                  },
-                  {
-                    name: "Naziya",
-                    title: "Dr.Ambedkar Institute of Technology",
-                    text: "This is Naziya from Bengaluru, currently studying in the 3rd year of BE in the ECE branch at Dr. AIT, Bengaluru. I recently completed a Full Stack Java course at Glowlogic Solutions, and it was a great learning experience. It was one of the best learning platforms for me, as it covered all the topics thoroughly and helped me become confident in Java. The schedule was very flexible and cooperative, allowing me to balance both my college work and the Java classes. The trainer was highly supportive and knowledgeable, explaining every concept clearly with practical examples. This course helped me build a strong foundation in Java and enhanced my skills significantly. I am truly thankful to the team for their continuous guidance and support throughout this wonderful journey.",
-                    img: "/Naziya.png",
                   },
                   {
                     name: "Thotakura Puneeth Sai",
@@ -428,6 +422,7 @@ const EventHub = () => {
                     name: "Srijib Dey",
                     title: "Working as a Employee",
                     text: "I have been doing a VLSI course from GlowLogics for the last 6 months. This course provided me with valuable exposure to the fundamentals and advanced concepts of VLSI design, including practical insights into design methodologies, simulation class techniques, and industry tools. The structured training, combined with hands-on tasks, helped me enhance my technical knowledge and problem-solving skills. ",
+                     img: "/Sri.png",
                   },
               ].map((item, index) => (
                 <div
